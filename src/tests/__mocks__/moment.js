ff904bba5b086ac45d6ca61466f3ad205ae80534
Johnattan Barona <johnattanbarona@gmail.com>
2019-05-21 08:40:44 -0400

    Init Commit

---
+++ src/tests/__mocks__/moment.js
@@ -0,0 +1,6 @@
+const moment = require("moment");
+
+
+export default (timeStamp = 0) => {
+    return moment(timeStamp);
+}
---
+++ src/tests/components/LoginPage.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import {LoginPage} from '../../components/LoginPage';
+
+test('should render Header correctly', () => {
+  const wrapper = shallow(<LoginPage/>);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+
+test('should call startLogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
+});
